f0ee2d81051b9ce23b7a29d39e28916849a5b97f
Mustafa Dokumaci <mustafa@foraa.com>
2017-04-25 17:24:26 +0200

    fix dns caching

--- test/index.js
+++ test/index.js
@@ -1,7 +1,21 @@
 'use strict'
 
 const test = require('tape')
+const ap = require('..')
 
-test('test', t => {
-  t.pass('done')
+test('github user', t => {
+  const github = ap('api.github.com')
+  github('GET', '/users/mstdokumaci')
+    .then(resp => {
+      t.equals(resp.login, 'mstdokumaci', 'correct username')
+      return github('GET', '/users/mstdokumaci')
+    })
+    .then(resp => {
+      t.equals(resp.login, 'mstdokumaci', 'correct username')
+      return github('GET', '/users/mstdokumaci')
+    })
+    .then(resp => {
+      t.equals(resp.login, 'mstdokumaci', 'correct username')
+      t.end()
+    })
 })
